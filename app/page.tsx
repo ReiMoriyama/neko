15691a50a32d80b753ed73dc19f0ca0271b7f684
ReiMoriyama <morichan0812@gmail.com>
2025-04-15 22:48:46 +0900

    猫の生態を移植

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,81 @@
-import Image from "next/image";
+"use client";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <>
+      <h1 className="pink">猫の実態</h1>
+      <p>
+        可愛い蘭ちゃん
+        <br />
+        大食いです
+        <br />
+        ふわふわ
+        <br />
+      </p>
+      <h2>猫は可愛い</h2>
+      <h2 className="blue">ランちゃんの名前は梅川蘭です。</h2>
+      <img src="/sakura.png" alt="sakura" />
+      <a href="https://google.com">google</a>で検索
+      <a href="https://google.com">
+        <img src="/sakura.png" alt="sakura" />
+      </a>
+      <table>
+        <tr>
+          <th>プラン名</th>
+          <th>料金</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>4</td>
+        </tr>
+      </table>
+      <table>
+        <tr>
+          <th>あああ</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>4</td>
+        </tr>
+      </table>
+      <p>名前：</p>
+      <input type="text" placeholder="苗字 名前" /> 性別：
+      <input type="radio" name="gender" value="男" checked />
+      <p>男</p>
+      <input type="radio" name="gender" value="女" />女
+      <input type="radio" name="gender" value="その他" />
+      その他 好きな色：
+      <input type="checkbox" name="color" value="red" checked />
+      <p>赤</p>
+      <input type="checkbox" name="color" value="red" />赤
+      <input type="checkbox" name="color" value="red" />赤
+      <input type="submit" value="送信" />
+      <select name="blood type">
+        <option value="A">A</option>
+        <option value="B">B</option>
+      </select>
+      <textarea name="messeage" placeholder="メッセージを入力"></textarea>
+      <div className="container">
+        <div className="item">Item 1</div>
+        <div className="item">Item 2</div>
+        <div className="item">Item 3</div>
+        <div className="item">Item 4</div>
+        <div className="item2">Item 1</div>
+        <ul className="container-ul">
+          <li>ss</li>
+          <li>aa</li>
+          <li>cc</li>
+        </ul>
+        <div className="item-1">Item 5</div>
+      </div>
+    </>
   );
 }
